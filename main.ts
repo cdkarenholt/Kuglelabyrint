66820a4f3720da81369a94d72f61daaa00d73b59
cdkarenholt <c.d.k.arenholt@gmail.com>
2025-09-03 13:47:53 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,23 @@
+pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+radio.setGroup(69)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    }
+    basic.clearScreen()
+    led.plot(pins.map(
+    pins.analogReadPin(AnalogReadWritePin.P1),
+    0,
+    1023,
+    0,
+    4
+    ), pins.map(
+    pins.analogReadPin(AnalogReadWritePin.P2),
+    0,
+    1023,
+    0,
+    4
+    ))
+    radio.sendNumber(pins.analogReadPin(AnalogReadWritePin.P1))
+    radio.sendValue("", pins.analogReadPin(AnalogReadWritePin.P2))
+})
